ffacbe3eb002a0a6ad6182f42a46ca5dbbb20f31
luca-terrazzan <luca.terraz@gmail.com>
2020-09-20 16:54:14 +0200

    added distribution calculation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.inland_simulator import InlandSimulator
 
 inland_simulator = InlandSimulator()
 
-for i in range(0, 100):
+for i in range(0, 10):
     inland_simulator.increase_year()
 
-print(inland_simulator)
+print(inland_simulator.inland_population.get_current_iq_distribution(true))
--- src/population.py
+++ src/population.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from src.individual import Individual
 
 
@@ -12,3 +13,18 @@ class Population():
     def remove_individuals(self, uids):
         for uid in uids:
             del self.individuals[uid]
+
+    def get_current_iq_distribution(self, draw: bool):
+        iqs = {}
+        for uid in self.individuals:
+            rounded_iq = round(self.individuals[uid].iq)
+            if rounded_iq in iqs:
+                iqs[rounded_iq] += 1
+            else:
+                iqs[rounded_iq] = 1
+
+        if draw:
+            plt.plot(iqs)
+            plt.ylabel("IQ")
+            plt.show()
+        return iqs
